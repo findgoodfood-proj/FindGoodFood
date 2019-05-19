e38fafc9dabf969bc64c2cd9eaac8bf12efb3360
Hayden Brown <hbrown10@binghamton.edu>
2019-05-19 21:15:49 +0000

    update food spec controller and add restaurant controller spec

--- spec/controllers/foods_controller_spec.rb
+++ spec/controllers/foods_controller_spec.rb
@@ -32,10 +32,6 @@ RSpec.describe FoodsController, type: :controller do
     {id: 1, restaurant_id: {"1" => "a"}, food: {name: "pasta", price: 1, tags: "a"}}
   }
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # FoodsController. Be sure to keep this updated too.
--- spec/controllers/restaurants_controller_spec.rb
+++ spec/controllers/restaurants_controller_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe RestaurantsController, type: :controller do
+  let(:valid_attributes) {
+    {id: 1, restaurant: {name: "Lost Dog", address: "a", phone_number: "123-4567", description: "desc"}}
+  }
 
+  let(:valid_session) { {user_id: 1} }
+  
+  let(:user1) {instance_double('User', id: 1, email: 'a@web.com')}
+
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a new Restaurant" do
+        allow(User).to receive(:find).and_return(user1)
+        post :create, valid_attributes, valid_session
+        expect(response).to redirect_to('/restaurants/1')
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    context "with valid params" do
+      let(:new_attributes) {
+        {id: 1, restaurant_id: {"1" => "a"}, food: {name: "pasta", price: 2, tags: "b"}}
+      }
+
+      it "updates the requested restaurant" do
+        allow(User).to receive(:find).and_return(user1)
+        restaurant = Restaurant.create! name: "Lost Dog", address: "a", phone_number: "123-4567", description: "desc"
+        put :update, {:id => 1, restaurant: {name: "Lost Dog", address: "a", phone_number: "123-4567", description: "changed description"}}, valid_session
+        restaurant.reload
+        expect(restaurant.description).to eq("changed description")
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "destroys the requested restaurant" do
+      allow(User).to receive(:find).and_return(user1)
+      restaurant = Restaurant.create! name: "Lost Dog", address: "a", phone_number: "123-4567", description: "desc", user_id: 1
+      expect {
+        delete :destroy, {:id => 1}, valid_session
+      }.to change(Restaurant, :count).by(-1)
+    end
+  end
 end
